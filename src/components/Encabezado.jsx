94bbd79729c924a7639731b1316339790794a253
4B31 <90332493+candelaochoa@users.noreply.github.com>
2021-09-15 16:26:03 -0300

    Segunda Parte (Encabezado, Footer, Tarjetas y Seccion Principal)

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Encabezado = () => {
-               return (
-                              <header>
-                                     Encabezado        
-                              </header>
-               )
-}
+  return (
+    <header>
+      <a href="">
+        <img src="" alt="icono" />
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://rickandmortyapi.com/documentation/">Docs</a>
+          </li>
+          <li>
+            <a href="https://rickandmortyapi.com/about/">About</a>
+          </li>
+          <li>
+            <a href="https://rickandmortyapi.com/help-us/">HELP US</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Encabezado
+export default Encabezado;
---
+++ src/components/PieDePagina.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const PieDePagina = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="">CHARACTERS</a>
+        </li>
+        <li>
+          <a href="">LOCATIONS</a>
+        </li>
+        <li>
+          <a href="">EPISODES</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="">SERVER STATUS</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="">
+            <img src="" alt="Git Hub" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="" alt="Twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="" alt="Help us" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default PieDePagina;
