3168f3b208dae7cd7363bbe695fc6a41dd6b4960
4B31 <90332493+candelaochoa@users.noreply.github.com>
2021-09-15 16:27:31 -0300

    Correccion App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Encabezado from "./components/Encabezado";
 import PieDePagina from "./components/PieDePagina";
 import SeccionPrincipal from "./components/SeccionPrincipal";
-import Tarjeta from "./components/Tarjeta";
 
 const App = () => {
   return (
     <>
       <Encabezado />
       <SeccionPrincipal />
-      <Tarjeta />
       <PieDePagina />
     </>
   );
